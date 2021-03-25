8382b42166767011c0d1858b13705d0868648029
Omar Jabri <omarjabri967@gmail.com>
2021-03-25 14:37:54 +0000

    added timestamp for each sensor output to define real time

--- src/sensors_test.cpp
+++ src/sensors_test.cpp
@@ -4,6 +4,8 @@
 #include <wiringPi.h>
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <ctime>
 #include <unistd.h>
 #define _USE_MATHS_DEFINES
 #include <math.h>
@@ -13,14 +15,20 @@ using namespace std;
 class hallSampleCallback : public SensorCallback{
     virtual void dataIn(double v){
         if(v != INIT){
+            auto time_now = chrono::system_clock::now();
+            time_t timestamp = chrono::system_clock::to_time_t(time_now);
             printf("Velocity: %f m/s\n", v);
+            cout << "TIMESTAMP HALL: " << ctime(&timestamp) << endl;
         }
     }
 };
 
 class sonarSampleCallback : public SensorCallback{
     virtual void dataIn(double t){
+        auto time_now = chrono::system_clock::now();
+        time_t timestamp = chrono::system_clock::to_time_t(time_now);
         printf("Distance: %f cm\n", t/58);
+        cout << "TIMESTAMP SONAR: " << ctime(&timestamp) << endl;
     }
 };
 
