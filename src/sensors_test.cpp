4aac932990cab277228dea31ddc94b4899517c48
Omar Jabri <omarjabri967@gmail.com>
2021-03-29 19:27:53 +0100

    added bluetooth integration, send distance data to laptop

--- src/sensors_test.cpp
+++ src/sensors_test.cpp
@@ -1,6 +1,7 @@
 #include "sensors_lib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <wiringPi.h>
 #include <iostream>
 #include <fstream>
@@ -9,8 +10,12 @@
 #include <unistd.h>
 #define _USE_MATHS_DEFINES
 #include <math.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
 using namespace std;
 #define INIT 2147483647
+#define UUID "163660a6-ad17-44fc-99c5-5c75e78ad815"
 
 /** Sample Call Back class inheriting SensorCallback, 
     associated with the hall effect sensor
@@ -42,6 +47,25 @@ class sonarSampleCallback : public SensorCallback{
         time_t timestamp = chrono::system_clock::to_time_t(time_now);
         printf("Distance: %f cm\n", t/58);
         cout << "TIMESTAMP SONAR: " << ctime(&timestamp) << endl;
+        double distance = t/58;
+        string data = to_string(distance);
+        const void *buffer = data.c_str();
+        //Send Data via bluetooth
+        struct sockaddr_rc addr = {0};
+        int s, status;
+        // char dest[18] = "14:D1:69:2F:E0:67"; // Android MAC addr
+        // char dest[18] = "78:31:c1:d7:19:1a"; // Laptop MAC addr
+        char dest[18] = "A0:AF:BD:8D:5A:53"; // VM MAC addr
+        s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+        addr.rc_family = AF_BLUETOOTH;
+        addr.rc_channel = (uint8_t) 1;
+        str2ba(dest, &addr.rc_bdaddr);
+        status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+        cout << "Status: " << status << endl;
+        if(status == 0){
+            status = write(s,buffer, sizeof(buffer));
+        }
+        close(s);
     }
 };
 
