8cc42c1943963a891f4c3f006966267b50f89032
Omar Jabri <omarjabri967@gmail.com>
2021-04-04 17:21:23 +0100

    added velocity estimation sensor

--- src/sensors_test.cpp
+++ src/sensors_test.cpp
@@ -1,5 +1,4 @@
 #include "sensors_lib.h"
-#include "fast_cgi_web_api.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +15,8 @@
 #include <jsoncpp/json/json.h>
 #define _USE_MATHS_DEFINES
 #include <math.h>
+#include <cstdio>
+#include <mutex>
 using namespace std;
 
 #define PORT_SONAR 8080
@@ -23,6 +24,13 @@ using namespace std;
 #define PORT_UDP 8888
 #define MAXLINE 1024
 char hostIp[MAXLINE];
+/** Set old distance to calculate the derivative and get velocity */
+volatile float old_distance = 0;
+volatile int upcoming_car = 1;
+volatile int distance_flag = 0;
+volatile int bike_flag = 0;
+volatile int velocity_flag = 0;
+std::mutex mtx;
 
 /** Sample Call Back class inheriting SensorCallback, 
     associated with the hall effect sensor
@@ -38,7 +46,12 @@ class hallSampleCallback : public SensorCallback{
       time_t timestamp = chrono::system_clock::to_time_t(time_now);
       printf("Velocity: %f m/s\n", v);
       cout << "TIMESTAMP HALL: " << ctime(&timestamp) << endl;
-      string data = to_string(v) + ", " + to_string(timestamp);
+      if(upcoming_car == 1 && v >= 8 && bike_flag == 0){
+          mtx.lock();
+          bike_flag = 1;
+          mtx.unlock();
+      }
+      string data = to_string(v) + "|" + to_string(timestamp);
       const void *buffer_data = data.c_str();
       /** Send sensor readings to app */
       struct sockaddr_in server_addr;
@@ -55,7 +68,6 @@ class hallSampleCallback : public SensorCallback{
       }
       else {
 	   send(sock, buffer_data, sizeof v, 0);
-	   //printf("HALL: Velocity sent\n");
 	   close(sock);
       }
     }
@@ -64,7 +76,7 @@ class hallSampleCallback : public SensorCallback{
 /** Sample Call Back class inheriting SensorCallback, 
     associated with the sonar sensor
  */
-class sonarSampleCallback : public SensorCallback{
+class sonarDistanceSampleCallback : public SensorCallback{
     /** Call back functin associated with sonar sensor to return the velocity readiings.
     */
     /** @param t - time of echo heading t obstacle and coming back as reading from sonar sensor
@@ -74,30 +86,52 @@ class sonarSampleCallback : public SensorCallback{
         time_t timestamp = chrono::system_clock::to_time_t(time_now);
         printf("Distance: %f cm\n", t/58);
         cout << "TIMESTAMP SONAR: " << ctime(&timestamp) << endl;
-	double distance = t/58;
-	string data = to_string(distance) + ", " + to_string(timestamp);
-	const void *buffer_data = data.c_str();
-	/** Send sensor readings to app */
-	int sock = 0, conn_status;
-	struct sockaddr_in server_addr;
-	char buffer[2048] = {0};
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(inet_pton(AF_INET, hostIp, &server_addr.sin_addr) <= 0) {
-	  printf("\nInvalid address/ Address not supported \n");
-	}
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(PORT_SONAR);
-	conn_status = connect(sock,(struct sockaddr *)&server_addr, sizeof(server_addr));
-	if(conn_status < 0){
-	  perror("ERROR connecting\n");
-	}
-        else {
-	  send(sock, buffer_data, sizeof distance, 0);
-	  //printf("SONAR: Distance sent\n");
-	  close(sock);
-	  }
-    }
-};
+        double distance = t/58;
+        if(upcoming_car == 1 && distance <= 100 && distance_flag == 0){
+          mtx.lock();
+          distance_flag = 1;
+          mtx.unlock();
+        }
+        string data = to_string(distance) + "|" + to_string(timestamp);
+        const void *buffer_data = data.c_str();
+        /** Send sensor readings to app */
+        int sock = 0, conn_status;
+        struct sockaddr_in server_addr;
+        char buffer[2048] = {0};
+        sock = socket(AF_INET, SOCK_STREAM, 0);
+        if(inet_pton(AF_INET, hostIp, &server_addr.sin_addr) <= 0) {
+          printf("\nInvalid address/ Address not supported \n");
+        }
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(PORT_SONAR);
+        conn_status = connect(sock,(struct sockaddr *)&server_addr, sizeof(server_addr));
+        if(conn_status < 0){
+          perror("ERROR connecting\n");
+        }
+              else {
+          send(sock, buffer_data, sizeof distance, 0);
+          close(sock);
+          }
+          }
+  };
+
+  class sonarVelocitySampleCallback : public SensorCallback {
+        virtual void dataIn(double t){
+          float v = (old_distance - (t/58))/10; //speed of incoming item
+          if(v >= 1){
+              auto time_now = chrono::system_clock::now();
+              time_t timestamp = chrono::system_clock::to_time_t(time_now);
+              printf("CAR VELOCITY: %f m/s\n", v);
+              cout << "TIMESTAMP VEL SONAR: " << ctime(&timestamp) << endl;
+              if(upcoming_car == 1 && v >= 10 && velocity_flag == 0){
+                mtx.lock();
+                velocity_flag = 1;
+                mtx.unlock();
+              }
+          }
+          old_distance = t/58;
+        }
+  };
 
 int main(int argc, char *argv[]){
   /** Get Phone address from UDP broadcast message*/
@@ -121,20 +155,28 @@ int main(int argc, char *argv[]){
     const int SONAR = 1;
     int pinInHall = 1;
     int pinOutHall = 4;
-    int pinInSonar = 24;
-    int pinOutSonar = 23;
-    Sensor* sonarSensor = new Sensor(&pinInSonar, &pinOutSonar);
+    int pinInSonarOne = 24;
+    int pinOutSonarOne = 23;
+    int pinInSonarTwo = 10;
+    int pinOutSonarTwo = 11;
+    Sensor* sonarSensorOne = new Sensor(&pinInSonarOne, &pinOutSonarOne);
+    Sensor* sonarSensorTwo = new Sensor(&pinInSonarTwo, &pinOutSonarTwo);
     Sensor* hallEffectSensor = new Sensor(&pinInHall,&pinOutHall);
-    sonarSampleCallback sonarCallback;
+    sonarDistanceSampleCallback sonarCallbackOne;
+    sonarVelocitySampleCallback sonarCallbackTwo;
     hallSampleCallback hallCallback;
-    sonarSensor->setCallBack(&sonarCallback);
+    sonarSensorOne->setCallBack(&sonarCallbackOne);
+    sonarSensorTwo->setCallBack(&sonarCallbackTwo);
     hallEffectSensor->setCallBack(&hallCallback);
-    sonarSensor->start(&pinInSonar, &pinOutSonar, SONAR);
+    // sonarSensorOne->start(&pinInSonarOne, &pinOutSonarOne, SONAR);
+    sonarSensorTwo->start(&pinInSonarTwo, &pinOutSonarTwo, SONAR);
     hallEffectSensor->start(&pinInHall, &pinOutHall, HALL);
     getchar();
-    sonarSensor->stop();
+    // sonarSensorOne->stop();
+    sonarSensorTwo->stop();
     hallEffectSensor->stop();
-    delete sonarSensor;
+    // delete sonarSensorOne;
+    delete sonarSensorTwo;
     delete hallEffectSensor;
     return 0;
 }
